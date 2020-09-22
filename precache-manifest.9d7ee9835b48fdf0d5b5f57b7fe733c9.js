72f4efa12ac886edd807ac53bf8914ad856fc130
Davi Santos <davi-rocha2@hotmail.com>
2020-09-22 10:35:55 -0300

    Updates

---
+++ precache-manifest.9d7ee9835b48fdf0d5b5f57b7fe733c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308fb5be6e198af74187aafb54148df1",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2dee8a92893d1abed878",
+    "url": "/react-monsters-rolodex/static/css/main.faadc137.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dee8a92893d1abed878",
+    "url": "/react-monsters-rolodex/static/js/main.1718ff95.chunk.js"
+  },
+  {
+    "revision": "84cac6f49c2db027c893",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.782a98e2.js"
+  }
+]);
